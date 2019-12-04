d619a93ecb3f151fc3beaf270f015aadddf70e01
Aldila Iskandar <aldila.iskndr@gmail.com>
2019-12-04 09:27:27 +0700

    [PROPS] created constant for boating dock app

---
+++ src/main/java/com/enigma/constant/CommandConstant.java
@@ -0,0 +1,11 @@
+package com.enigma.constant;
+
+public class CommandConstant {
+    public static final String CREATE_BOATING_DOCK = "create_boating_dock";
+    public static final String DOCK = "dock";
+    public static final String LEAVE = "leave";
+    public static final String STATUS = "status";
+    public static final String REG_NUMBERS_FOR_BOATS_WITH_COLOUR = "registration_numbers_for_boats_with_colour";
+    public static final String SLOT_NUMBERS_FOR_BOATS_WITH_COLOUR ="slot_numbers_for_boats_with_colour";
+    public static final String SLOT_NUMBER_FOR_REG_NUMBER = "slot_number_for_registration_number";
+}
---
+++ src/main/java/com/enigma/constant/MessageConstant.java
@@ -0,0 +1,13 @@
+package com.enigma.constant;
+
+public class MessageConstant {
+    public static final String CREATED_BOATING_DOCK ="Created a boating dock with %s piers";
+    public static final String DOCK_SUCCESS = "Allocated pier number: %d";
+    public static final String DOCK_FAIL = "Sorry, boating dock is full";
+    public static final String LEAVE = "Pier number %d is free";
+    public static final String STATUS_HEAD_NUMBER = "Pier No.";
+    public static final String STATUS_HEAD_REG_NUMBER = "Registration No.";
+    public static final String STATUS_HEAD_COLOUR = "Colour";
+    public static final String NOT_FOUND = "Not Found";
+
+}
